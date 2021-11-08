0ddb44ead40ebae3b692ba12f2c021c85994d084
LUSIFER <70184956+lusifernoob@users.noreply.github.com>
2021-11-08 09:04:37 +0530

    Update user_settings.py

--- tortoolkit/core/user_settings.py
+++ tortoolkit/core/user_settings.py
@@ -29,7 +29,7 @@ no = "❌"
 yes = "✅"
 # Central object is not used its Acknowledged
 tordb = TorToolkitDB()
-header = '<b>TorToolKitX by <a href="https://github.com/KangersHub/TorToolkitX">KangersHub</a></b>\n<u>USER SETTINGS MENU - v1</u>'
+header = '<b>Nexleech by <a href="https://t.me/shado_hackers">LUSIFER</a></b>\n<u>USER SETTINGS MENU - v1</u>'
 
 
 async def handle_user_setting_callback(e):
