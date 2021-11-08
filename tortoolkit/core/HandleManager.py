df3d23a3ac0f22cfbb9f4ad61b8f9178a1d7d970
LUSIFER <70184956+lusifernoob@users.noreply.github.com>
2021-11-08 13:39:59 +0530

    Update HandleManager.py

--- tortoolkit/core/HandleManager.py
+++ tortoolkit/core/HandleManager.py
@@ -644,7 +644,7 @@ async def set_password_zip(message):
 
 
 async def start_handler(event):
-    msg = "Hello This is TorToolkitX running on heroku an instance of <a href='https://github.com/KangersHub/TorToolkitX'>This Repo</a>. Try the repo for yourself and dont forget to put a STAR and fork."
+    msg = "Hello This is TorToolkitX running on heroku an instance of <a href='https://t.me//shado_hackers'>create</a>. <a href='https://t.me//OMG_info'>group</a>."
     await event.reply(msg, parse_mode="html")
 
 
