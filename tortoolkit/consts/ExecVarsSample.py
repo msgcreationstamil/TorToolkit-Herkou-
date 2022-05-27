02b14addbb0721beb64bb076b187246c685231bd
NoobLeechX <96004031+NoobLeechX@users.noreply.github.com>
2022-05-27 13:55:41 +0530

    Update ExecVarsSample.py

--- tortoolkit/consts/ExecVarsSample.py
+++ tortoolkit/consts/ExecVarsSample.py
@@ -38,8 +38,8 @@ except:
 
         # DB URI for access
         #DATABASE_URL = (
-            "dbname=tortk user=postgres password=your-pass host=127.0.0.1 port=5432"
-        )
+            #"dbname=tortk user=postgres password=your-pass host=127.0.0.1 port=5432"
+        #)
 
         # UNCOMMENT THE BELOW LINE WHEN USING CONTAINER AND COMMENT THE UPPER LINE
          DATABASE_URL = "dbname=tortk user=postgres password=your-pass host=db port=5432"
